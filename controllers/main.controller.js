a8aaf3da834e3ab2142bd19b46f90f80b05835b6
Andriy Cherevko <cherevkin@gmail.com>
2018-05-19 21:44:13 +0200

    updated routes (added post methods)

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,51 +1,48 @@
-const ToDo = require('../models/db');
+const ToDo = require("../models/db");
 
 function getUsersFromDB(req, res) {
-    ToDo.find({}, (err, users)=>{
-        if(err){
-            res.send(404);
-            res.send('User not found');
-        }
-        res.send(users);
-    })
-    };
+  ToDo.find({}, (err, users) => {
+    if (err) {
+      res.send(404);
+      res.send("User not found");
+    }
+    res.send(users);
+  });
+}
 
+function getDataFromDB(req, res) {
+  ToDo.find({}, (err, users) => {
+    if (err) {
+      res.send(404);
+      res.send("User not found");
+    }
+    res.send(users);
+  });
+}
 
-    function getDataFromDB(req, res) {
-        ToDo.find({}, (err, users)=>{
-            if(err){
-                res.send(404);
-                res.send('User not found');
-            }
-            res.send(users);
-        })
-        };
+function showSingle(req, res) {
+  ToDo.find({}, (err, users) => {
+    if (err) {
+      res.send(404);
+      res.send("User not found");
+    }
+    res.send(users);
+  });
+}
 
-        function showSingle(req, res) {
-            ToDo.find({}, (err, users)=>{
-                if(err){
-                    res.send(404);
-                    res.send('User not found');
-                }
-                res.send(users);
-            })
-            };
+function create(req, res) {
+  ToDo.find({}, (err, users) => {
+    if (err) {
+      res.send(404);
+      res.send("User not found");
+    }
+    res.send(users);
+  });
+}
 
-            function create(req, res) {
-                ToDo.find({}, (err, users)=>{
-                    if(err){
-                        res.send(404);
-                        res.send('User not found');
-                    }
-                    res.send(users);
-                })
-                };
-
-    module.exports = {
-        getUsersFromDB,
-        getDataFromDB,
-        showSingle,
-        create
-    };
- 
-    
+module.exports = {
+  getUsersFromDB,
+  getDataFromDB,
+  showSingle,
+  create
+};
